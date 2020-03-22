51ed840d35b0fe392678deaec33d24842ce76018
cppIsTheBest <hugo.boulanger@etu.enseeiht.fr>
2020-03-22 13:42:31 +0100

    Add line / column nb and add parser unit tests

--- src/compiler/lexer.rs
+++ src/compiler/lexer.rs
@@ -1,15 +1,46 @@
 use std::fs::File;
 use std::io;
 use std::io::{BufReader, Read, Seek, SeekFrom};
+use std::fmt;
 
 static DELIMITERS: [char; 5] = ['{', '}', '(', ')', ','];
 static SINGLE_CHAR_OPERATORS: [char; 2] = ['<', '>'];
 static TWO_CHAR_OPERATORS: [&str; 6] = ["&&", "||", "==", "!=", "<=", ">="];
 static OPERATOR_FIRST_CHARS: [char; 6] = ['&', '|', '=', '!', '<', '>'];
 
+pub struct Token {
+    pub str: String,
+    pub line: u32,
+    pub column: u32
+}
+
+impl Token {
+    fn new(str: String, lexer: &Lexer) -> Token {
+        let column = if lexer.current_char_in_token { lexer.current_column } else { lexer.previous_column };
+        let line = if lexer.current_char == '\n' { lexer.previous_line } else { lexer.current_line };
+        Token {
+            str,
+            line,
+            column
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "\"{}\" - line {}, column {}", self.str, self.line, self.column)
+    }
+}
+
 pub struct Lexer<'a> {
     reader: BufReader<File>,
-    file_name: &'a str
+    file_name: &'a str,
+    previous_line: u32,
+    previous_column: u32,
+    current_line: u32,
+    current_column: u32,
+    current_char_in_token: bool,
+    current_char: char
 }
 
 impl<'a> Lexer<'a> {
@@ -18,11 +49,17 @@ impl<'a> Lexer<'a> {
         let reader = BufReader::new(file);
         Ok(Lexer {
             reader,
-            file_name
+            file_name,
+            previous_line: 1,
+            previous_column: 0,
+            current_line: 1,
+            current_column: 0,
+            current_char_in_token: false,
+            current_char: '\n'
         })
     }
 
-    pub fn get_next_token(&mut self) -> Result<String, String> {
+    pub fn get_next_token(&mut self) -> Result<Token, String> {
         // Read until a not-whitespace parameter is found.
         let mut c = self.read_char()?;
         while c.is_ascii_whitespace() && c != '\u{0}' {
@@ -31,7 +68,7 @@ impl<'a> Lexer<'a> {
 
         // The token is a single delimiter character.
         if DELIMITERS.contains(&c) {
-            return Ok(c.to_string());
+            return Ok(Token::new(c.to_string(), &self));
         }
 
         // The token seems to be an operator.
@@ -43,7 +80,7 @@ impl<'a> Lexer<'a> {
         self.get_number_or_id_token(c)
     }
 
-    fn get_operator_token(&mut self, c: char) -> Result<String, String> {
+    fn get_operator_token(&mut self, c: char) -> Result<Token, String> {
         let mut token = String::new();
         token.push(c);
         let expected_char = match c {
@@ -57,7 +94,7 @@ impl<'a> Lexer<'a> {
 
         // The token is a two-characters operator
         if c2 == expected_char {
-            return Ok(token);
+            return Ok(Token::new(token, &self));
         }
         // The token is a single character operator ('<' or '>')
         else if (c == '<' || c == '>')
@@ -67,18 +104,16 @@ impl<'a> Lexer<'a> {
                 || DELIMITERS.contains(&c2)
                 || OPERATOR_FIRST_CHARS.contains(&c2)) {
             token.pop();
-            if let Err(error) = self.reader.seek(SeekFrom::Current(-1)) {
-                return Err(format!("Could not get token. Cause : {:?}", error));
-            }
-            return Ok(token);
+            self.rewind_char()?;
+            return Ok(Token::new(token, &self));
         }
         // The token starts as an operator but not one
         else {
-            return Err(format!("Invalid token {}. Note : recognized operators are {:?} and {:?}.", token, SINGLE_CHAR_OPERATORS, TWO_CHAR_OPERATORS));
+            return Err(format!("Invalid token {}. Note : recognized operators are {:?} and {:?}.", Token::new(token, &self), SINGLE_CHAR_OPERATORS, TWO_CHAR_OPERATORS));
         }
     }
 
-    fn get_number_or_id_token(&mut self, first_char: char) -> Result<String, String> {
+    fn get_number_or_id_token(&mut self, first_char: char) -> Result<Token, String> {
         let is_token_number = first_char.is_ascii_digit();
         let is_token_identifier = first_char.is_ascii_alphabetic();
         let mut rewind_one_char = false;
@@ -114,36 +149,64 @@ impl<'a> Lexer<'a> {
         // The token is not a valid number or identifier
         if failure {
             return
-                if is_token_number { Err(format!("Invalid token {}. It starts with a digit but is not a number.", token)) }
-                else { Err(format!("Invalid token {}. It contains illegal characters.", token)) }
+                if is_token_number { Err(format!("Invalid token {}. It starts with a digit but is not a number.", Token::new(token, &self))) }
+                else { Err(format!("Invalid token {}. It contains illegal characters.", Token::new(token, &self))) }
         }
 
         // No token found and we reached end-of-file
         if token.len() == 0 && c == '\u{0}' {
-            return Ok(String::new())
+            return Ok(Token::new(String::new(), &self))
         }
 
-        // The last character is nor part of the token, we just have to remove it and we are good.
+        // The last character is nor part of the token, we just have to un-read it and we are good.
         if rewind_one_char {
             token.pop();
-            if let Err(error) = self.reader.seek(SeekFrom::Current(-1)) {
-                return Err(format!("Could not get token. Cause : {:?}", error));
-            }
-        }
+            self.rewind_char()?;
+       }
 
         // Token is a valid number or identifier
-        Ok(token)
+        Ok(Token::new(token, &self))
     }
 
     fn read_char(&mut self) -> Result<char, String> {
         let mut buffer = [0; 1];
         match self.reader.read(&mut buffer) {
-            Ok(_) => return Ok(buffer[0] as char),
-            Err(e) => return Err(format!("Cannot read character from file {}. Cause : {:?}", self.file_name, e))
+            Ok(_) => {
+                self.current_char = buffer[0] as char;
+                self.current_char_in_token = !(self.current_char.is_ascii_whitespace() || self.current_char == '\u{0}');
+                if self.current_char == '\n' {
+                    self.previous_line = self.current_line;
+                    self.previous_column = self.current_column;
+                    self.current_line += 1;
+                    self.current_column = 0;
+                } else {
+                    self.previous_column = self.current_column;
+                    self.current_column += 1;
+                }
+            }
+            Err(e) => {
+                return Err(format!("Cannot read character from file {} (line {}, column {}). Cause : {:?}",
+                                   self.file_name, self.current_line, self.current_column, e))
+            }
         }
+        Ok(buffer[0] as char)
+    }
+
+    fn rewind_char(&mut self) -> Result<(), String> {
+        if let Err(error) = self.reader.seek(SeekFrom::Current(-1)) {
+            return Err(format!("Could not get token (line {}, column {}). Cause : {:?}",
+                        self.previous_line, self.previous_column, error));
+        }
+        if self.current_char == '\n' {
+            self.current_line = self.previous_line;
+        }
+        self.current_column = self.previous_column;
+        self.current_char_in_token = true;
+        Ok(())
     }
 }
 
+// TODO Add tests for line and columns feature, current tests are not enough
 #[cfg(test)]
 mod tests {
     use crate::compiler::lexer::{Lexer, SINGLE_CHAR_OPERATORS, TWO_CHAR_OPERATORS};
@@ -169,28 +232,28 @@ mod tests {
     }
 
     fn check_benchmark_output(lexer: &mut Lexer) {
-        assert_eq!(lexer.get_next_token().unwrap(), "th15I5AnAlphanum3r1cId3nt1f1er");
-        assert_eq!(lexer.get_next_token().unwrap(), "thisTooAndNextUpIsANumber");
-        assert_eq!(lexer.get_next_token().unwrap(), "123456");
-        assert_eq!(lexer.get_next_token().unwrap(), "<");
-        assert_eq!(lexer.get_next_token().unwrap(), ">");
-        assert_eq!(lexer.get_next_token().unwrap(), "test");
-        assert_eq!(lexer.get_next_token().unwrap(), "<=");
-        assert_eq!(lexer.get_next_token().unwrap(), ">=");
-        assert_eq!(lexer.get_next_token().unwrap(), "&&");
-        assert_eq!(lexer.get_next_token().unwrap(), "||");
-        assert_eq!(lexer.get_next_token().unwrap(), "==");
-        assert_eq!(lexer.get_next_token().unwrap(), "!=");
-        assert_eq!(lexer.get_next_token().unwrap(), "test");
-        assert_eq!(lexer.get_next_token().unwrap(), ",");
-        assert_eq!(lexer.get_next_token().unwrap(), "test");
-        assert_eq!(lexer.get_next_token().unwrap(), "(");
-        assert_eq!(lexer.get_next_token().unwrap(), ")");
-        assert_eq!(lexer.get_next_token().unwrap(), "{");
-        assert_eq!(lexer.get_next_token().unwrap(), "}");
-        assert_eq!(lexer.get_next_token().unwrap(), "test");
-        assert!(lexer.get_next_token().unwrap().is_empty());
-        assert!(lexer.get_next_token().unwrap().is_empty());
+        assert_eq!(lexer.get_next_token().unwrap().str, "th15I5AnAlphanum3r1cId3nt1f1er");
+        assert_eq!(lexer.get_next_token().unwrap().str, "thisTooAndNextUpIsANumber");
+        assert_eq!(lexer.get_next_token().unwrap().str, "123456");
+        assert_eq!(lexer.get_next_token().unwrap().str, "<");
+        assert_eq!(lexer.get_next_token().unwrap().str, ">");
+        assert_eq!(lexer.get_next_token().unwrap().str, "test");
+        assert_eq!(lexer.get_next_token().unwrap().str, "<=");
+        assert_eq!(lexer.get_next_token().unwrap().str, ">=");
+        assert_eq!(lexer.get_next_token().unwrap().str, "&&");
+        assert_eq!(lexer.get_next_token().unwrap().str, "||");
+        assert_eq!(lexer.get_next_token().unwrap().str, "==");
+        assert_eq!(lexer.get_next_token().unwrap().str, "!=");
+        assert_eq!(lexer.get_next_token().unwrap().str, "test");
+        assert_eq!(lexer.get_next_token().unwrap().str, ",");
+        assert_eq!(lexer.get_next_token().unwrap().str, "test");
+        assert_eq!(lexer.get_next_token().unwrap().str, "(");
+        assert_eq!(lexer.get_next_token().unwrap().str, ")");
+        assert_eq!(lexer.get_next_token().unwrap().str, "{");
+        assert_eq!(lexer.get_next_token().unwrap().str, "}");
+        assert_eq!(lexer.get_next_token().unwrap().str, "test");
+        assert!(lexer.get_next_token().unwrap().str.is_empty());
+        assert!(lexer.get_next_token().unwrap().str.is_empty());
    }
 
     #[test]
@@ -206,39 +269,39 @@ mod tests {
         let mut lexer = Lexer::new(OPERATOR_TYPO_FILE).unwrap();
         match lexer.get_next_token() {
             Err(error) => assert_eq!(error, format!(
-                "Invalid token |-. Note : recognized operators are {:?} and {:?}.", SINGLE_CHAR_OPERATORS, TWO_CHAR_OPERATORS)),
+                "Invalid token \"|-\" - line 1, column 2. Note : recognized operators are {:?} and {:?}.", SINGLE_CHAR_OPERATORS, TWO_CHAR_OPERATORS)),
             _ => assert!(false),
         }
-        assert_eq!(lexer.get_next_token().unwrap(), "thisTokenShouldBeReadWithoutIssues");
+        assert_eq!(lexer.get_next_token().unwrap().str, "thisTokenShouldBeReadWithoutIssues");
     }
 
     #[test]
     fn tokenize_number_with_illegal_char_fails() {
         let mut lexer = Lexer::new(NB_WITH_ILLEGAL_CHAR_FILE).unwrap();
         match lexer.get_next_token() {
-            Err(error) => assert_eq!(error, "Invalid token 3.14. It starts with a digit but is not a number."),
+            Err(error) => assert_eq!(error, "Invalid token \"3.14\" - line 1, column 4. It starts with a digit but is not a number."),
             _ => assert!(false),
         }
-        assert_eq!(lexer.get_next_token().unwrap(), "thisTokenShouldBeReadWithoutIssues");
+        assert_eq!(lexer.get_next_token().unwrap().str, "thisTokenShouldBeReadWithoutIssues");
     }
 
     #[test]
     fn tokenize_number_with_alphabetic_fails() {
         let mut lexer = Lexer::new(NB_WITH_ALPHABETIC_FILE).unwrap();
         match lexer.get_next_token() {
-            Err(error) => assert_eq!(error, "Invalid token 10O0. It starts with a digit but is not a number."),
+            Err(error) => assert_eq!(error, "Invalid token \"10O0\" - line 1, column 4. It starts with a digit but is not a number."),
             _ => assert!(false),
         }
-        assert_eq!(lexer.get_next_token().unwrap(), "thisTokenShouldBeReadWithoutIssues");
+        assert_eq!(lexer.get_next_token().unwrap().str, "thisTokenShouldBeReadWithoutIssues");
     }
 
     #[test]
     fn tokenize_id_with_illegal_char_fails() {
         let mut lexer = Lexer::new(ID_WITH_ILLEGAL_CHAR_FILE).unwrap();
         match lexer.get_next_token() {
-            Err(error) => assert_eq!(error, "Invalid token hello_world. It contains illegal characters."),
+            Err(error) => assert_eq!(error, "Invalid token \"hello_world\" - line 1, column 11. It contains illegal characters."),
             _ => assert!(false),
         }
-        assert_eq!(lexer.get_next_token().unwrap(), "thisTokenShouldBeReadWithoutIssues");
+        assert_eq!(lexer.get_next_token().unwrap().str, "thisTokenShouldBeReadWithoutIssues");
     }
 }
--- src/compiler/parser.rs
+++ src/compiler/parser.rs
@@ -1,4 +1,4 @@
-use crate::compiler::lexer::Lexer;
+use crate::compiler::lexer::{Token, Lexer};
 
 pub enum ComparisonOperator {
     Greater,
@@ -30,45 +30,10 @@ pub enum NextCondition {
     NextTransition(Box<Transition>)
 }
 
-fn parse_next_condition(lexer: &mut Lexer) -> Result<NextCondition, String> {
-    let token = lexer.get_next_token()?;
-    if let Some(boolean_operator) = to_boolean_operator(&token) {
-        Ok(NextCondition::NextCondition(boolean_operator, Box::new(parse_condition(lexer)?)))
-    }
-    else if token == ")" {
-        expect(lexer, vec![","])?;
-        Ok(NextCondition::NextTransition(Box::new(parse_transitions(lexer)?)))
-    }
-    else {
-        Err(format!("Expected either a boolean operator \"&&\", \"||\" or a \")\" token, found {}", token))
-    }
-}
-
 pub enum Condition {
     QuantityCondition(String, ComparisonOperator, u8, NextCondition),
     PositionCondition(NeighborCell, String, NextCondition),
     True(NextCondition)
-}
-
-fn parse_condition(lexer: &mut Lexer) -> Result<Condition, String> {
-    let token = lexer.get_next_token()?;
-    if token == "true" {
-        Ok(Condition::True(parse_next_condition(lexer)?))
-    }
-    else if let Some(neighbor_cell) = to_neighbor_cell(&token) {
-        expect_is(lexer)?;
-        let state_name = expect_identifier(lexer)?;
-        Ok(Condition::PositionCondition(neighbor_cell, state_name, parse_next_condition(lexer)?))
-    }
-    else if is_identifier(&token) {
-        let comparison_operator = expect_comparison_operator(lexer)?;
-        let number = expect_neighbor_number(lexer)?;
-        Ok(Condition::QuantityCondition(token, comparison_operator, number, parse_next_condition(lexer)?))
-    }
-    else {
-        Err(format!("Expected either token \"true\", a neighbor cell identifier \
-            (one of \"A\", \"B\", \"C\", \"D\", \"E\", \"F\", \"H\"), or an alphanumeric identifier, but found {}", token))
-    }
 }
 
 pub enum Transition {
@@ -76,23 +41,23 @@ pub enum Transition {
     End
 }
 
-fn parse_transitions(lexer: &mut Lexer) -> Result<Transition, String> {
-    let token = expect(lexer, vec!["(", "}"])?;
-    if token == "(" {
-        let initial_state_name = expect_identifier(lexer)?;
-        expect(lexer, vec![","])?;
-        let next_state_name = expect_identifier(lexer)?;
-        expect(lexer, vec![","])?;
-        Ok(Transition::Transition(initial_state_name, next_state_name, Box::new(parse_condition(lexer)?)))
-    }
-    else {
-        Ok(Transition::End)
-    }
-}
-
 pub enum State {
     State(String, u8, u8, u8, Box<State>),
     Next(Transition)
+}
+
+type Ast = State;
+
+pub fn parse(file_name: &str) -> Result<Ast, String> {
+    let mut lexer: Lexer;
+    match Lexer::new(file_name) {
+        Ok(l) => { lexer = l; },
+        Err(io_error) => { return Err(format!("Cannot parse file {}. Cause : {:?}", file_name, io_error)); }
+    };
+
+    expect(&mut lexer, vec!["states"])?;
+    expect(&mut lexer, vec!["{"])?;
+    parse_state(&mut lexer)
 }
 
 fn parse_state(lexer: &mut Lexer) -> Result<State, String> {
@@ -115,92 +80,135 @@ fn parse_state(lexer: &mut Lexer) -> Result<State, String> {
     }
 }
 
-type Ast = State;
+fn parse_transitions(lexer: &mut Lexer) -> Result<Transition, String> {
+    let token = expect(lexer, vec!["(", "}"])?;
+    if token == "(" {
+        let initial_state_name = expect_identifier(lexer)?;
+        expect(lexer, vec![","])?;
+        let next_state_name = expect_identifier(lexer)?;
+        expect(lexer, vec![","])?;
+        Ok(Transition::Transition(initial_state_name, next_state_name, Box::new(parse_condition(lexer)?)))
+    }
+    else {
+        Ok(Transition::End)
+    }
+}
 
-pub fn parse(file_name: &str) -> Result<Ast, String> {
-    let mut lexer: Lexer;
-    match Lexer::new(file_name) {
-        Ok(l) => { lexer = l; },
-        Err(io_error) => { return Err(format!("Cannot parse file {}. Cause : {:?}", file_name, io_error)); }
-    };
+fn parse_condition(lexer: &mut Lexer) -> Result<Condition, String> {
+    let token = lexer.get_next_token()?;
+    if token.str == "true" {
+        Ok(Condition::True(parse_next_condition(lexer)?))
+    }
+    else if let Some(neighbor_cell) = to_neighbor_cell(&token) {
+        expect_is(lexer)?;
+        let state_name = expect_identifier(lexer)?;
+        Ok(Condition::PositionCondition(neighbor_cell, state_name, parse_next_condition(lexer)?))
+    }
+    else if is_identifier(&token) {
+        let comparison_operator = expect_comparison_operator(lexer)?;
+        let number = expect_neighbor_number(lexer)?;
+        Ok(Condition::QuantityCondition(token.str, comparison_operator, number, parse_next_condition(lexer)?))
+    }
+    else {
+        Err(format!("Expected either token \"true\", a neighbor cell identifier \
+            (one of \"A\", \"B\", \"C\", \"D\", \"E\", \"F\", \"H\"), or an alphanumeric identifier, but found {}.", token))
+    }
+}
 
-    expect(&mut lexer, vec!["states"])?;
-    expect(&mut lexer, vec!["{"])?;
-    parse_state(&mut lexer)
+fn parse_next_condition(lexer: &mut Lexer) -> Result<NextCondition, String> {
+    let token = lexer.get_next_token()?;
+    if let Some(boolean_operator) = to_boolean_operator(&token) {
+        Ok(NextCondition::NextCondition(boolean_operator, Box::new(parse_condition(lexer)?)))
+    }
+    else if token.str == ")" {
+        expect(lexer, vec![","])?;
+        Ok(NextCondition::NextTransition(Box::new(parse_transitions(lexer)?)))
+    }
+    else {
+        Err(format!("Expected either a boolean operator \"&&\", \"||\" or a \")\" token, found {}.", token))
+    }
 }
 
 /// Return the next token if it's one of the expected tokens, or raises an error.
 fn expect(lexer: &mut Lexer, expected: Vec<&str>) -> Result<String, String> {
+    let mut expected_as_sentence = String::new();
     let token = lexer.get_next_token()?;
     for i in 0..expected.len() {
-        if token == expected[i] {
-            return Ok(token);
+        if token.str == expected[i] {
+            return Ok(token.str);
         }
+        if i != 0 {
+            expected_as_sentence.push_str(" or ");
+        }
+        expected_as_sentence.push_str("\"");
+        expected_as_sentence.push_str(expected[i]);
+        expected_as_sentence.push_str("\"");
     }
-    Err(format!("Expected {:?}, found {}", expected, token))
+    Err(format!("Expected {}, found {}.", expected_as_sentence, token))
 }
 
 /// Return the next token if it's an alphanumeric identifier, or raises an error.
 fn expect_identifier(lexer: &mut Lexer) -> Result<String, String> {
     let token = lexer.get_next_token()?;
     if is_identifier(&token) {
-        Ok(token)
+        Ok(token.str)
     }
     else {
-        Err(format!("Expected an alphanumeric identifier, found {}", token))
+        Err(format!("Expected an alphanumeric identifier, found {}.", token))
     }
 }
 
-fn is_identifier(token: & String) -> bool {
-    token.chars().all(|c| c.is_ascii_alphanumeric()) && !token.chars().all(|c| c.is_ascii_digit())
+fn is_identifier(token: & Token) -> bool {
+    token.str.chars().all(|c| c.is_ascii_alphanumeric())
+        && !token.str.chars().all(|c| c.is_ascii_digit())
 }
 
 /// Return the next token translated into an integer between 0 and 255 if possible, or raises an error.
 fn expect_u8(lexer: &mut Lexer) -> Result<u8, String> {
     let token = lexer.get_next_token()?;
-    match token.parse::<u8>() {
+    match token.str.parse::<u8>() {
         Ok(number) => Ok(number),
-        Err(_) => Err(format!("Expected an integer between 0 and 255, found {}", token))
+        Err(_) => Err(format!("Expected an integer between 0 and 255, found {}.", token))
     }
 }
 
 /// Return the next token translated into an integer between 0 and 8 if possible, or raises an error.
 fn expect_neighbor_number(lexer: &mut Lexer) -> Result<u8, String> {
     let token = lexer.get_next_token()?;
-    if let Ok(number) = token.parse::<u8>() {
+    if let Ok(number) = token.str.parse::<u8>() {
         if number <= 8 {
             return Ok(number);
         }
     }
-    Err(format!("Expected an integer between 0 and 8, found {}", token))
+    Err(format!("Expected an integer between 0 and 8, found {}.", token))
 }
 
 /// Return success if the next token is 'is', or raises an error.
 fn expect_is(lexer: &mut Lexer) -> Result<(), String> {
     let token = lexer.get_next_token()?;
-    match token.as_str() {
+    match token.str.as_str() {
         "is" => Ok(()),
-        _ => Err(format!("Expected \"is\" token, found {}", token))
+        _ => Err(format!("Expected \"is\" token, found {}.", token))
     }
 }
 
 /// Return a comparison operator if the next token represents one, or raises an error.
 fn expect_comparison_operator(lexer: &mut Lexer) -> Result<ComparisonOperator, String> {
     let token = lexer.get_next_token()?;
-    match token.as_str() {
+    match token.str.as_str() {
         "<" => Ok(ComparisonOperator::Lesser),
         ">" => Ok(ComparisonOperator::Greater),
         "<=" => Ok(ComparisonOperator::LesserOrEqual),
         ">=" => Ok(ComparisonOperator::GreaterOrEqual),
         "==" => Ok(ComparisonOperator::Equal),
         "!=" => Ok(ComparisonOperator::Different),
-        _ => Err(format!("Expected one of \"<\", \">\", \"<=\", \">=\", \"==\", or \"!=\" tokens, found {}", token))
+        _ => Err(format!("Expected one of \"<\", \">\", \"<=\", \">=\", \"==\", or \"!=\" tokens, found {}.", token))
     }
 }
 
 /// Translate the token into a boolean operator, if possible.
-fn to_boolean_operator(token: & String) -> Option<BooleanOperator> {
-    match token.as_str() {
+fn to_boolean_operator(token: & Token) -> Option<BooleanOperator> {
+    match token.str.as_str() {
         "&&" => Some(BooleanOperator::And),
         "||" => Some(BooleanOperator::Or),
         _ => None
@@ -208,8 +216,8 @@ fn to_boolean_operator(token: & String) -> Option<BooleanOperator> {
 }
 
 /// Translate the token into a neighbor cell identifier, if possible.
-fn to_neighbor_cell(token: & String) -> Option<NeighborCell> {
-    match token.as_str() {
+fn to_neighbor_cell(token: & Token) -> Option<NeighborCell> {
+    match token.str.as_str() {
         "A" => Some(NeighborCell::A),
         "B" => Some(NeighborCell::B),
         "C" => Some(NeighborCell::C),
@@ -219,5 +227,105 @@ fn to_neighbor_cell(token: & String) -> Option<NeighborCell> {
         "G" => Some(NeighborCell::G),
         "H" => Some(NeighborCell::H),
         _ => None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::compiler::parser::parse;
+
+    static BENCHMARK_FILE: &str = "resources/tests/parser_benchmark.txt";
+    static NON_EXISTING_FILE: &str = "resources/tests/does_not_exist.txt";
+    static COND_ERROR_FILE: &str = "resources/tests/parser_condition_error.txt";
+    static EXPECT_COMP_OP_FILE: &str = "resources/tests/parser_expected_comparison_operator.txt";
+    static EXPECT_ID_FILE: &str = "resources/tests/parser_expected_identifier.txt";
+    static EXPECT_IS_FILE: &str = "resources/tests/parser_expected_is_token.txt";
+    static EXPECT_NEIGHBOR_NB_FILE: &str = "resources/tests/parser_expected_neighbor_number.txt";
+    static EXPECT_U8_FILE: &str = "resources/tests/parser_expected_u8.txt";
+    static NEXT_COND_ERROR_FILE: &str = "resources/tests/parser_next_condition_error.txt";
+    static NO_STATES_FILE: &str = "resources/tests/parser_no_states_keyword.txt";
+
+    #[test]
+    fn parse_benchmark_succeeds() {
+        match parse(BENCHMARK_FILE) {
+            Ok(_) => assert!(true),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn parse_non_existing_file_fails() {
+         match parse(NON_EXISTING_FILE) {
+            Err(error) => {
+                assert!(error.contains("Cannot parse file resources/tests/does_not_exist.txt. Cause : "));
+                assert!(error.contains("No such file or directory"));
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn parse_condition_error_fails() {
+        match parse(COND_ERROR_FILE) {
+            Err(error) => assert_eq!(error, "Expected either token \"true\", a neighbor cell identifier (one of \"A\", \"B\", \"C\", \"D\", \"E\", \"F\", \"H\"), \
+            or an alphanumeric identifier, but found \"3153\" - line 7, column 22."),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn parse_expect_comp_operator_fails() {
+        match parse(EXPECT_COMP_OP_FILE) {
+            Err(error) => assert_eq!(error, "Expected one of \"<\", \">\", \"<=\", \">=\", \"==\", or \"!=\" tokens, found \"plouf\" - line 7, column 29."),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn parse_expect_identifier_fails() {
+         match parse(EXPECT_ID_FILE) {
+            Err(error) => assert_eq!(error, "Expected an alphanumeric identifier, found \"51566\" - line 3, column 10."),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn parse_expect_is_token_fails() {
+         match parse(EXPECT_IS_FILE) {
+            Err(error) => assert_eq!(error, "Expected \"is\" token, found \"plouf\" - line 8, column 39."),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn parse_expect_neigbhor_number_fails() {
+         match parse(EXPECT_NEIGHBOR_NB_FILE) {
+            Err(error) => assert_eq!(error, "Expected an integer between 0 and 8, found \"22\" - line 7, column 28."),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn parse_expect_u8_fails() {
+         match parse(EXPECT_U8_FILE) {
+            Err(error) => assert_eq!(error, "Expected an integer between 0 and 255, found \"260\" - line 2, column 15."),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn parse_next_condition_error_fails() {
+         match parse(NEXT_COND_ERROR_FILE) {
+            Err(error) => assert_eq!(error, "Expected either a boolean operator \"&&\", \"||\" or a \")\" token, found \"dead\" - line 8, column 46."),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn parse_no_states_keyword_fails() {
+         match parse(NO_STATES_FILE) {
+            Err(error) => assert_eq!(error, "Expected \"states\", found \"plouf\" - line 1, column 5."),
+            _ => assert!(false)
+        }
     }
 }
